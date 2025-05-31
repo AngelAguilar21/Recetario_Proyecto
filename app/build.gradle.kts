e4e7c4be16368cfa36b6b136c41d59f036787b63
Kheyla Condor <23100185@ue.edu.pe>
2025-05-30 20:13:32 -0500

    feat(generar listado de recetas) style(mejor visualizacion para la pantalla home) test(añadir funcion de ir a descripcion de receta al presionar una opcion en home, añadir boton de mirar todas las recetas, añadir funcion de buscar y filtrar, añadir pantallas de categorias y generar listado de categorias, cambiar las categorias en relacion a la data del api) perf(mejorar tiempo de carga de las listas de recetas)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,13 @@ dependencies {
     //Add navigation compose
     implementation("androidx.navigation:navigation-compose:2.4.0")
     implementation("androidx.compose.material:material-icons-extended")
+
+    // Retrofit para llamadas HTTP
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // ViewModel y Lifecycle
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+
 }
